02a20acc21a26d0f9d357803ef7ba3325d6acfc8
keshew <artyom.korotckov2017@yandex.ru>
2025-10-30 14:19:27 +0500

    Initial Commit

---
+++ Egg Planner/Egg_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_PlannerApp.swift
+//  Egg Planner
+//
+//  Created by Артём Коротков on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Egg_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
